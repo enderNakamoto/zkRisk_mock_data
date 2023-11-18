44d36b412ecb3f9267b593b03097acf6038e4d61
enderNakamoto <ender.nakamoto@gmail.com>
2023-11-18 05:05:09 +0300

    added signature

--- src/app/api/vehicle_data/route.ts
+++ src/app/api/vehicle_data/route.ts
@@ -1,6 +1,15 @@
 import { NextRequest, NextResponse } from 'next/server';
-
 import { vehicle_data } from '@/lib/vehicle_data';
+
+// import { Buffer } from 'safe-buffer';
+// import { BigInteger } from 'bigi';
+// import {schnorr} from 'bip-schnorr';
+
+const Buffer = require('safe-buffer').Buffer; 
+const BigInteger = require('bigi');
+const schnorr = require('bip-schnorr');
+const convert = schnorr.convert;
+
 
 // Implement toJSON for BigInt so we can include values in response
 (BigInt.prototype as any).toJSON = function () {
@@ -8,7 +17,9 @@ import { vehicle_data } from '@/lib/vehicle_data';
 };
 
 function getVehicleData(carId: string){
-  // const privateKey = "EKFLHx273j3WU9rJg6ndchQgoZ4Hk1cp35bhrZYM8ivrjb3LmNYM"
+  // const PRIVATE_KEY = "1zkp2wdKteRJeFKm4dEgGBHDbKCRk39WQ6MQWp95xQwWRCqC"
+  const PRIVATE_KEY_HEX = 'B7E151628AED2A6ABF7158809CF4F3C762E7160F38B4DA56A784D9045190CFEF';
+
 
   let fast_driver = false
   let high_mileage = false
@@ -29,7 +40,14 @@ function getVehicleData(carId: string){
   }
 
   const result = vehicle_data(carId, high_mileage, fast_driver);
-  
+
+// let privKeyHex = BigInteger.fromHex(PRIVATE_KEY);
+const odometer= BigInt(result["vehicle_state"]["odometer"])
+const odometerHex = odometer.toString(16)
+//const message = Buffer.from(odometerHex, 'hex');
+const message = Buffer.from('243F6A8885A308D313198A2E03707344A4093822299F31D0082EFA98EC4E6C89', 'hex');
+const createdSignature = schnorr.sign(PRIVATE_KEY_HEX, message);
+
   // const signature = client.signFields(
   //   [BigInt(carId), BigInt(result["vehicle_state"]["odometer"])],
   //   privateKey
@@ -42,7 +60,8 @@ function getVehicleData(carId: string){
   // }
 
   return {
-    "vehicle_data": result
+    "vehicle_data": result, 
+    "signature": createdSignature.toString('hex')
   }
 }
 
